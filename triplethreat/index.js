7d2f7afd3fd5177d9b106efff89ea17b174d9c6c
Nelly <nkhmeleva@yahoo.com>
2020-03-11 23:11:46 -0600

    solved

--- triplethreat/index.js
+++ triplethreat/index.js
@@ -1,15 +1,14 @@
 // return true if array has 3 ints in increasing order
 
 function triplethreat(array) {
-    var len = array.length;
-    for(var i=0; i<len-2; i++){
-        if(array[i+1]==array[i]+1){
-           if(array[i+2]== array[i]+2){
+    for(var i=0; i<array.length-2; i++){
+        if(array[i+1] === array[i]+1){
+           if(array[i+2] === array[i]+2){
               return true
            }
         }
     }
-    return false;
+    return false
 }
 
 module.exports = triplethreat
